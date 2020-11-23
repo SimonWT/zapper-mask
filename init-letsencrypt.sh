2d8ffe842e5bc061eed6a3d06dd42f731913280b
Yaroslav Yudinskikh <yaskih@mail.ru>
2020-11-23 18:07:56 +0300

    upd

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(arwow.ru www.arwow.ru)
+domains=(arwow.ru)
 rsa_key_size=4096
 data_path="./nginx/data/certbot"
 email="mryudinskikh@gmail.com" # Adding a valid address is strongly recommended
